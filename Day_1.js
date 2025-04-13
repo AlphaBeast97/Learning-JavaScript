0ab3da56ba19071df8505857eb53e8212bd4fb51
AlphaBeast97 <ksaad5272@gmail.com>
2025-04-13 23:13:03 +0500

    Day 1 of Java script

---
+++ Day_1.js
@@ -0,0 +1,51 @@
+// fname = "M. Saad Khan";
+// // console.log(fname);
+// // age = 2000;
+// // console.log(age);
+// // age = null;
+// // console.log(age);
+
+
+// let age;
+// age = 200;
+// console.log(age);
+
+// const bye = "bye";
+// console.log(bye);
+
+
+// is = false;
+// console.log(is);
+
+// // object
+// // const object key is updatable
+// const Student = {
+//     FullName : "Saad Khan",
+//     isPass : true,
+//     cgpa : 3.74,
+//     age : 200
+// };
+// Student.age *= 2;
+
+// console.log(Student);
+
+//* actual code *//
+// 1st ques
+const Product = {
+    ProdName: "Parker Jotter  Standard CT Ball Pen (black)",
+    ProdRating: 4.5,
+    isDeal: true,
+    ProdPrice: 270,
+    ProdOffer: 5
+};
+console.log(Product);
+// 2nd ques
+const Profile = {
+    name: "M. Saad Khan",
+    posts: 45,
+    isfollow: true,
+    followers: 97,
+    following: 45,
+    description: "Hobby of Photography trying to be a web dev."
+};
+    console.log(Profile);
